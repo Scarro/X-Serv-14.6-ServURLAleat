74b1ec2d386b646683e8628b13020af6f557865a
Sergio Carro Albarrán <sergioca22@gmail.com>
2015-05-06 01:06:20 +0200

    terminado

---
+++ aleat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import random
+import webapp
+
+class urlAleatorias(webapp.webApp):
+    
+    def process(self,parsedRequest):
+        link = str(random.randrange(1000000000))
+        return ("200 OK", "<html><body><h1>Hola</h1>" +
+                        "<a href='" + link + "'>Dame otra</a>" +
+                        "</body></html>" + "\r\n")
+
+if __name__ == "__main__":
+    url = urlAleatorias("localhost",8000)
